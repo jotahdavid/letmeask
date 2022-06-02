1ec246573c6266288fa7b4a1b167f4e7e3076fe1
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-06-01 21:03:00 -0300

    chore: add firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  databaseURL: import.meta.env.DATABASE_URL,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const database = getDatabase(app);
